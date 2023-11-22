81db3c95147db1d4f477d8d3647f988f835459d8
d8ahazard <d8ahazard@gmail.com>
2023-11-22 08:58:42 -0600

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -16,9 +16,11 @@ def build_ui():
     with gr.Blocks() as auto_console:
         gr.Markdown("## Simple BASH Console Emulator")
         gr.Markdown("Enter any command to execute. **Warning**: This interface allows all commands and should be used with caution.")
-        input_text = gr.Textbox(label="Command")
-        output_text = gr.Textbox(label="Output", interactive=False)
-        input_text.change(fn=bash_command, inputs=input_text, outputs=output_text)
-    return ((auto_console, "Shellex", "shellex"),)
+        with gr.Row():
+            input_text = gr.Textbox(label="Command")
+            submit_input = gr.Button(label="Submit")
+        output_text = gr.Textbox(label="Output", interactive=False, lines=10)
+        submit_input.click(fn=bash_command, inputs=input_text, outputs=output_text)
+        return ((auto_console, "Shellex", "shellex"),)
 
 script_callbacks.on_ui_tabs(build_ui)
